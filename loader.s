1f7b16f6c0a00c741283dde0498ca3214e46daa6
ruben <rubenmmm3@gmail.com>
2016-11-03 18:39:53 +1300

    call external C function kmain() successfully
    loader setps up and takesdown the stack frame properly

--- loader.s
+++ loader.s
@@ -18,10 +18,21 @@ align 4							; code must be 4 byte aligned
 	dd CHECKSUM					; and the checksum
 
 loader:							; loader label (defined as entry point in linker script)
-	mov eax, 0xCAFEBABE				; place number 0xCAFEBABE in register eax
-	mov esp, kernel_stack + KERNEL_STACK_SIZE	; point esp to start of stack (end of memory area)
-	mov word [0x000B8000], 0x8241			; write ascii 'A', green on grey bg to top left of screen
-	mov word ebx, [0x000b8000]			; copy value there to register
+	mov	eax, 0xCAFEBABE				; place number 0xCAFEBABE in register eax
+	mov	esp, kernel_stack + KERNEL_STACK_SIZE	; point esp to start of stack (end of memory area)
+	
+	extern	kmain
+	
+	push	ebp					; set up stack frame
+	mov	ebp, esp
+
+	call  	kmain					; result of kmain will b e in eax
+	
+	mov	esp, ebp				;takedown stack frame
+	pop	ebp					;same as 'leave' op
+	
+	;mov word [0x000B8000], 0x8241			; write ascii 'A', green on grey bg to top left of screen
+	;mov word ebx, [0x000b8000]			; copy value there to register
 
 .loop:
 	jmp .loop					;loop forever
