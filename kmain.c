8f306262bab08d1f47ad759466890e1fa141f538
ruben <rubenmmm3@gmail.com>
2016-11-03 18:36:47 +1300

    write wrapper for writing to the framebuffer
    loader.s calls external C function kmain() successfully
    kmain() calls fb_write_cell. currently the hex code is backwards,
    this needs to be fixed

--- kmain.c
+++ kmain.c
@@ -1,5 +1,30 @@
+#define FB_GREEN	2
+#define FB_DARK_GREY	8
 
+char *fb;
+
+/** fb_write_cell:
+ *  writes a character with the given foreground and background to position i
+ *  in the framebuffer
+ *
+ *  @param i the location in the framebuffer
+ *  @param c the character to write
+ *  @param fg the foreground color
+ *  @param bg the background color
+ */
+void fb_write_cell (unsigned int i, char c, unsigned char fg, unsigned char bg)
+{
+	fb[i] = c;
+	fb[i + 1] = ((fg & 0x0f) << 4) | (bg & 0x0f);
+}
 int kmain () 
 {
+	fb = (char *) 0x000B8000; // init framebuffer
+	
+	fb_write_cell (0, 'A', FB_GREEN, FB_DARK_GREY);
+	fb_write_cell (3, 'B', FB_GREEN, FB_DARK_GREY);
 	return 0;
 }
+
+//TODO: make seperate init function
+//	seperate fb functions into seperate function
