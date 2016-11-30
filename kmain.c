1b4c2d0bb255d2e1081f3d40c66af0cc119c2942
ruben <rubenmmm3@gmail.com>
2016-11-30 20:20:05 +1300

    implement clear screen function
    a color test is also added but only used for testing

--- kmain.c
+++ kmain.c
@@ -1,14 +1,22 @@
-#define FB_BLACK	0
-#define FB_BLUE		1
-#define FB_GREEN	2
-#define FB_CYAN		3
-#define FB_RED		4
-#define FB_MAGENTA	5
-#define FB_BROWN	6
-#define FB_LIGHT_GREY	7
-#define FB_DARK_GREY	8
+#define FB_BLACK		0
+#define FB_BLUE			1
+#define FB_GREEN		2
+#define FB_CYAN			3	
+#define FB_RED			4
+#define FB_MAGENTA		5
+#define FB_BROWN		6
+#define FB_LIGHT_GREY		7
+#define FB_DARK_GREY		8
+#define FB_LIGHT_BLUE		9
+#define FB_LIGHT_GREEN		10
+#define FB_LIGHT_CYAN		11
+#define FB_LIGHT_RED		12
+#define FB_LIGHT_MAGENTA 	13
+#define FB_LIGHT_BROWN		14
+#define FB_WHITE		15
 
-#define FB_WHITE	'f'
+#define FB_WIDTH	80
+#define FB_HEIGHT	25
 
 char *fb = (char *) 0x000B8000; // starting address of framebuffer
 
@@ -24,30 +32,59 @@ char *fb = (char *) 0x000B8000; // starting address of framebuffer
 void fb_write_cell (unsigned int i, char c, unsigned char fg, unsigned char bg)
 {
 	unsigned int loc = 2*i;
-	fb[loc] = c;
-
-	//just insert character there, not color 
+	fb[loc] = c; 
 	fb[loc+1] = ((bg & 0x0f) << 4) | (fg & 0x0f);
-
 }
+
 /** fb_clear_all:
  *  clears entire framebuffer by filling it with spaces 
  *  
  *  @param bg color to set framebufer to
  */
-//void fb_clear_all(unsigned char bg)
-//{
-	
-//}
+void fb_clear_all(unsigned char bg)
+{
+	unsigned int i;
+	for ( i = 0; i < FB_WIDTH * FB_HEIGHT; i++)
+	{
+		fb_write_cell (i, ' ', FB_WHITE, bg);
+	}
+}
+void colortest()
+{
+	fb_write_cell (161, 'c', FB_LIGHT_GREY, FB_BLACK);
+	fb_write_cell (162, 'o', FB_LIGHT_GREY, FB_BLACK);
+	fb_write_cell (163, 'l', FB_LIGHT_GREY, FB_BLACK);
+	fb_write_cell (164, 't', FB_LIGHT_GREY, FB_BLACK);
+	fb_write_cell (165, 'e', FB_LIGHT_GREY, FB_BLACK);
+	fb_write_cell (166, 's', FB_LIGHT_GREY, FB_BLACK);
+	fb_write_cell (167, 't', FB_LIGHT_GREY, FB_BLACK);
 
-
+	//foreground		
+	unsigned int col; 
+	for(col = 0; col < 16; col++)
+	{
+		fb_write_cell(col, 'A', col, FB_BLACK);
+	}
+	//background		
+	 
+	for(col = 0; col < 16; col++)
+	{
+		fb_write_cell(col+ 16, 'A', FB_BLACK, col);
+	}	
+}
+void corners()
+{
+	fb_write_cell (0, '#', FB_GREEN, FB_BLACK);
+	fb_write_cell (79, '#', FB_GREEN, FB_BLACK);
+	fb_write_cell (1920, '#', FB_GREEN, FB_BLACK);
+	fb_write_cell (1999, '#', FB_GREEN, FB_BLACK);
+}
 int kmain () 
 {
-	fb_write_cell (0, 'A', FB_GREEN, FB_BLACK);
-	//fb_write_cell (80, 'A', FB_GREEN, FB_BLACK);
-	fb_write_cell (1999, 'A', FB_GREEN, FB_BLACK);
-	fb_write_cell (1920, 'A', FB_GREEN, FB_BLACK);
-	fb_write_cell (79, 'A', FB_GREEN, FB_BLACK);
+	fb_clear_all(FB_BLACK);	
+	corners();
+	//colortest();
+	
 	return 0;
 }
 
